150edb368fb2878a5c2fe45e6deb9a671a7a08af
Wellerson Roberto <wellerson010@hotmail.com>
2017-07-14 00:39:04 -0300

    Adicionado componente de about

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Todo extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        );
+    }
+}
