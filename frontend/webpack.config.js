fcaf5256bd8f86a439ec12b3a3b804f1c7ca206a
Wellerson Roberto <wellerson010@hotmail.com>
2017-07-14 00:30:38 -0300

    Primeiro commit"

---
+++ frontend/webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8081,
+        contentBase: './public'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        rules: [
+            {
+                test: /.js[x]?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [require('babel-plugin-transform-object-rest-spread')]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+                use: 'file-loader'
+            }
+        ]
+    }
+}
